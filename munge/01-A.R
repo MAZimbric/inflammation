a8255f7e53b6b4ef83ab682801c3f817f3656df1
Madalyn Zimbric <madalyn.zimbric@gmail.com>
2016-08-11 11:22:46 -0400

    giving up on the factor issue
    explanation of data cleaning, commenting out non-working code.

--- munge/01-A.R
+++ munge/01-A.R
@@ -8,27 +8,27 @@ extract_useful <- function(data, samples) {
 inflammation.reads1 <- extract_useful(inflammation.reads1, inflammation.reads1$Sample)
 inflammation.reads2 <- extract_useful(inflammation.reads2, inflammation.reads2$Sample)
 
-#In order to merge the sets, we need to deal with the < entries and convert the marker values to numerics
-rm_less_thans <- function(data_column){
-  less_thans <- grep(pattern = "<", as.character(data_column), value = FALSE)
-  data_column[less_thans] <- NA
-  return(data_column)
-}
-
-f <- colwise(rm_less_thans)
-inflammation.reads1 <- f(inflammation.reads1)
-inflammation.reads2 <- f(inflammation.reads2)
-
-#function to deal with lack in R, factor to numeric conversion
-factor.to.numeric <- function (d) {
-  d <- as.numeric(as.character(d))
-  return(d)
-}
-  
-#this code does not work
-inflammation.reads1[3:ncol(inflammation.reads1)] <- ddply(inflammation.reads1[3:ncol(inflammation.reads1)], is.factor == TRUE, factor.to.numeric)
-inflammation.reads2 <- ddply(inflammation.reads2, 3:27, factor.to.numeric)
-
+# The followinwg problem was dealt with by naughty actions on the xlsx file. See README
+# #In order to merge the sets, we need to deal with the < entries and convert the marker values to numerics
+# rm_less_thans <- function(data_column){
+#   less_thans <- grep(pattern = "<", as.character(data_column), value = FALSE)
+#   data_column[less_thans] <- NA
+#   return(data_column)
+# }
+# 
+# f <- colwise(rm_less_thans)
+# inflammation.reads1 <- f(inflammation.reads1)
+# inflammation.reads2 <- f(inflammation.reads2)
+# 
+# #function to deal with lack in R, factor to numeric conversion
+# factor.to.numeric <- function (d) {
+#   d <- as.numeric(as.character(d))
+#   return(d)
+# }
+#   
+# #this code does not work
+# inflammation.reads1[3:ncol(inflammation.reads1)] <- ddply(inflammation.reads1[3:ncol(inflammation.reads1)], is.factor == TRUE, factor.to.numeric)
+# inflammation.reads2 <- ddply(inflammation.reads2, 3:27, factor.to.numeric)
 
 
 #The following translates the sample code into something useable
