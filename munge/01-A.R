67637be695258037d8b11fb4697ba29a8016840f
MZimbric <madalyn.zimbric@gmail.com>
2016-08-14 14:20:50 -0400

    initialize TODO, rm broken code from munging script

--- munge/01-A.R
+++ munge/01-A.R
@@ -10,29 +10,6 @@ extract_useful <- function(data, samples) {
 #and now we call them on the two sets
 inflammation.data <- extract_useful(inflammation.data, inflammation.data$Sample)
 
-# The followinwg problem was dealt with by naughty actions on the xlsx file. See README
-# #In order to merge the sets, we need to deal with the < entries and convert the marker values to numerics
-# rm_less_thans <- function(data_column){
-#   less_thans <- grep(pattern = "<", as.character(data_column), value = FALSE)
-#   data_column[less_thans] <- NA
-#   return(data_column)
-# }
-# 
-# f <- colwise(rm_less_thans)
-# inflammation.reads1 <- f(inflammation.reads1)
-# inflammation.reads2 <- f(inflammation.reads2)
-# 
-# #function to deal with lack in R, factor to numeric conversion
-# factor.to.numeric <- function (d) {
-#   d <- as.numeric(as.character(d))
-#   return(d)
-# }
-#   
-# #this code does not work
-# inflammation.reads1[3:ncol(inflammation.reads1)] <- ddply(inflammation.reads1[3:ncol(inflammation.reads1)], is.factor == TRUE, factor.to.numeric)
-# inflammation.reads2 <- ddply(inflammation.reads2, 3:27, factor.to.numeric)
-
-
 #The following translates the sample code into something useable
 #The initial digit represents the number of days the sample was stored 
 inflammation.data$storage_days <- str_match(inflammation.data$Sample, "^[:digit:][:digit:]?")
