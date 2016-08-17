04309e1f42a840e7d46a6cdcf2922daa18511d6e
Madalyn Zimbric <madalyn.zimbric@gmail.com>
2016-08-17 10:56:25 -0400

    basic plotting loop to be debugged

--- src/patient-age-marker-plots.R
+++ src/patient-age-marker-plots.R
@@ -7,4 +7,20 @@ source(file = "src/helpers.R")
 #with patient age as the independent variable and marker level as the 
 #dependent variable.
 
+plot.clinical <- function(clinical) {
+  markers <- names(clinical[10:ncol(clinical)])
+  patients <- levels(as.factor(combined.clinical$retro_ID))
+  for (i in seq_along(markers)) {
+    for (j in patients){
+      #subset by patient id
+      plot.data <- subset(clinical, clinical$retro_id == j)
+      
+      patient.plot <- ggplot(plot.data, aes(x=SP_age, y = markers[i])) +
+        geom_point() +
+        geom_line() +
+        geom_vline(X1st_NTM_age[1])
+    }
+  }
+}
 
+plot.clinical(combined.clinical)
