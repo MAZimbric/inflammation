86c6e9ccbeed511938496cf6d6ab391c856144a3
Madalyn Zimbric <madalyn.zimbric@gmail.com>
2016-08-17 15:04:20 -0400

    attempts at solving the all-NA column bug

--- src/patient-age-marker-plots.R
+++ src/patient-age-marker-plots.R
@@ -7,23 +7,27 @@ source(file = "src/helpers.R")
 #with patient age as the independent variable and marker level as the 
 #dependent variable.
 
-plot.clinical <- function(clinical) {
+plot.clinical <- function(clinical){
   markers <- names(clinical[10:ncol(clinical)])
   patients <- levels(as.factor(combined.clinical$retro_ID))
   for (i in seq_along(markers)) {
     for (j in patients){
       #subset by patient id
       plot.data <- clinical[clinical$retro_ID == j,]
-      plot.data <- drop.na.column(plot.data)
-      divider <- plot.data$X1st_NTM_age[1] 
       
-      patient.plot <- ggplot(plot.data, aes_string(x= "SP_age", y = markers[i])) +
-        geom_point() +
-        geom_line() +
-        geom_vline(xintercept = divider)
+      if (all(is.na(clinical[markers[i]]))) next
       
-      ggsave(patient.plot, filename=paste("figures/clinical/preliminary-plot-", markers[i], "patient-", j, ".png",sep=""))
-    }
+      else {
+        divider <- plot.data$X1st_NTM_age[1] 
+      
+        patient.plot <- ggplot(plot.data, aes_string(x= "SP_age", y = markers[i])) +
+          geom_point() +
+          geom_line() +
+          geom_vline(xintercept = divider, color = "red", linetype = "dashed")
+      
+        ggsave(patient.plot, filename=paste("figures/clinical/preliminary-plot-", markers[i], "patient-", j, ".png",sep=""))
+      }
+    }  
   }
 }
 
