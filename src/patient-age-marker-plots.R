538c97f810199d844e3e6bf23b7ee22025a696f9
Madalyn Zimbric <madalyn.zimbric@gmail.com>
2016-08-31 13:33:31 -0400

    comment out headers

--- src/patient-age-marker-plots.R
+++ src/patient-age-marker-plots.R
@@ -1,7 +1,7 @@
-setwd("D://mzimbric/Desktop/Projects/inflammation")
-library(ProjectTemplate)
-load.project()
-source(file = "src/helpers.R")
+# setwd("D://mzimbric/Desktop/Projects/inflammation")
+# library(ProjectTemplate)
+# load.project()
+# source(file = "src/helpers.R")
 
 #This file will create plots for levels of markers in clinical patients, 
 #with patient age as the independent variable and marker level as the 
